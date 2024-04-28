9953ce13b9fdf19c00a566ddd03500dd8537a78e
faustozamparelli <fausto.zamparelli@gmail.com>
2024-04-28 11:49:33 +0200

    tmux scripts

---
+++ .config/tmux/tmux-dp1.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+
+# Fuzzy search for files and directories
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    # First, search for files and directories at depth 1 in ~/ and ~/.config/
+    result=$(find ~/ ~/.config/ ~/Downloads/ ~/Developer/ /Volumes/SD/ -mindepth 1 -maxdepth 1 -type d | fzf --expect=ctrl-space)
+    key=$(head -1 <<< "$result")
+    selected=$(tail -1 <<< "$result")
+fi
+
+# Exit if no selection was made
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+# Get the name of the selected directory or file
+name=$(basename "$selected")
+
+# Check if a window with the same name already exists
+if tmux list-windows -F "#{window_name}" | grep -q "^$name$"; then
+    # If a window with the same name exists, switch to that window
+    tmux select-window -t "$name"
+    exit 0
+fi
+
+# Determine if the selection is a file or directory
+if [[ -d $selected ]]; then
+    if [[ $key == 'ctrl-space' ]]; then
+        # If Ctrl-Space was pressed, cd into the directory in the current window
+        tmux send-keys "cd $selected" C-m
+        tmux send-keys 'C-s' C-m
+    else
+        # If directory, open a new tmux window with the directory name and cd into the directory
+        tmux new-window -n "$name" -c "$selected"
+        sleep 0.3
+        tmux send-keys 'C-s' C-m
+    fi
+elif [[ -f $selected ]]; then
+    # If file, determine the action based on the key pressed
+    parent_dir=$(dirname "$selected")
+    if [[ $key == 'ctrl-space' ]]; then
+        # If Ctrl-Space was pressed, open the file in the current window
+        tmux send-keys "cd $parent_dir && nvim $(basename "$selected")" C-m
+    else
+        # If Enter was pressed, open a new tmux window with the file name, cd into the parent directory, and open the file in nvim
+        tmux new-window -n "$name" -c "$parent_dir"
+        tmux send-keys "nvim $(basename "$selected")" C-m
+    fi
+fi
+
+# If the script is being run from nvim, switch to the first free window and run the script from there
+if [[ $NVIM_LISTEN_ADDRESS ]]; then
+    tmux select-window -t 1
+    tmux send-keys './tmux-sessionzer.sh' C-m
+fi
---
+++ .config/tmux/tmux-recursive.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+# Recursive fuzzy search for files and directories
+if [[ $# -eq 1 ]]; then
+    selected=$1
+else
+    result=$(find ~/Developer/ ~/Downloads/ /Volumes/SD/ ~/.config/fish/ ~/.config/tmux/ ~/.config/nvim/lua/ ~/.config/git/ ~/.config/yabai/ ~/.config/wezterm/ ~/.config/goku/ /usr/local/bin/ ~/.config/sketchybar/ -mindepth 1 -type f | fzf --expect=ctrl-space)
+    key=$(head -1 <<< "$result")
+    selected=$(tail -1 <<< "$result")
+fi
+
+# Exit if no selection was made
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+# Get the name of the selected directory or file
+name=$(basename "$selected")
+
+# Determine if the selection is a file or directory
+if [[ -d $selected ]]; then
+    if [[ $key == 'ctrl-space' ]]; then
+        # If Ctrl-Space was pressed, cd into the directory in the current window
+        tmux send-keys "cd $selected" C-m
+        tmux send-keys 'C-s' C-m
+    else
+        # If directory, open a new tmux window with the directory name and cd into the directory
+        tmux new-window -n "$name" -c "$selected"
+        sleep 0.3
+        tmux send-keys 'C-s' C-m
+    fi
+elif [[ -f $selected ]]; then
+    # If file, determine the action based on the key pressed
+    parent_dir=$(dirname "$selected")
+    if [[ $key == 'ctrl-space' ]]; then
+        # If Ctrl-Space was pressed, open the file in the current window
+        tmux send-keys "cd $parent_dir && nvim $(basename "$selected")" C-m
+    else
+        # If Enter was pressed, open a new tmux window with the file name, cd into the parent directory, and open the file in nvim
+        tmux new-window -n "$name" -c "$parent_dir"
+        tmux send-keys "nvim $(basename "$selected")" C-m
+    fi
+fi
+
+# If the script is being run from nvim, switch to the first free window and run the script from there
+if [[ $NVIM_LISTEN_ADDRESS ]]; then
+    tmux select-window -t 1
+    tmux send-keys './tmux-sessionzer.sh' C-m
+fi
