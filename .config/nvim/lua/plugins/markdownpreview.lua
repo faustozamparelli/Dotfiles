aff6ac7e388187f34e89e9be3f1b623fac141f7e
faustozamparelli <fausto.zamparelli@gmail.com>
2024-05-06 00:14:14 +0200

    added some plugins

---
+++ .config/nvim/lua/plugins/markdownpreview.lua
@@ -0,0 +1,11 @@
+return {
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" },
+	build = function()
+		vim.fn["mkdp#util#install"]()
+	end,
+	config = function()
+		vim.keymap.set("n", "<C-m><C-p>", "<cmd>MarkdownPreviewToggle<cr>", { silent = true })
+	end,
+}
