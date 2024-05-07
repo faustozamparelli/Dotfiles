cee580b2fc528be1d54ea6d23ad84bc54971c3a0
faustozamparelli <fausto.zamparelli@gmail.com>
2024-05-07 15:31:04 +0200

    new tmux renaming funtionality

--- .config/tmux/tmux-dp1.sh
+++ .config/tmux/tmux-dp1.sh
@@ -38,10 +38,9 @@ else
             sleep 0.3
             tmux send-keys 'C-s' C-m
         else
-            # If Enter was pressed, cd into the directory in the current window and rename the window
+            # If Enter was pressed, cd into the directory in the current window
             tmux send-keys "cd $selected" C-m
             tmux send-keys 'C-s' C-m
-            tmux rename-window "$name"
         fi
     elif [[ -f $selected ]]; then
         # If file, determine the action based on the key pressed
@@ -51,9 +50,8 @@ else
             tmux new-window -n "$name" -c "$parent_dir"
             tmux send-keys "nvim $(basename "$selected")" C-m
         else
-            # If Enter was pressed, open the file in the current window and rename the window
+            # If Enter was pressed, open the file in the current window
             tmux send-keys "cd $parent_dir && nvim $(basename "$selected")" C-m
-            tmux rename-window "$name"
         fi
     fi
 fi
--- .config/tmux/tmux-recursive.sh
+++ .config/tmux/tmux-recursive.sh
@@ -31,10 +31,9 @@ if [[ -d $selected ]]; then
         sleep 0.3
         tmux send-keys 'C-s' C-m
     else
-        # If Enter was pressed, cd into the directory in the current window and rename the window
+        # If Enter was pressed, cd into the directory in the current window
         tmux send-keys "cd $selected" C-m
         tmux send-keys 'C-s' C-m
-        tmux rename-window "$name"
     fi
 elif [[ -f $selected ]]; then
     # If file, determine the action based on the key pressed
@@ -44,8 +43,7 @@ elif [[ -f $selected ]]; then
         tmux new-window -n "$name" -c "$parent_dir"
         tmux send-keys "nvim $(basename "$selected")" C-m
     else
-        # If Enter was pressed, open the file in the current window and rename the window
+        # If Enter was pressed, open the file in the current window
         tmux send-keys "cd $parent_dir && nvim $(basename "$selected")" C-m
-        tmux rename-window "$name"
     fi
 fi
