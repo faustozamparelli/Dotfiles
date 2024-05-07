4afcb161a984e2b66bdc3e9e48e9d35f7e9fb942
faustozamparelli <fausto.zamparelli@gmail.com>
2024-05-07 12:06:08 +0200

    changed config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -22,6 +22,7 @@ if status is-interactive
   set -Ua fish_user_paths $PYENV_ROOT/bin
   set -Ua fish_user_paths $JAVA_HOME/bin
   set -Ua fish_user_paths /opt/homebrew/opt/openvpn/sbin
+  set -Ua fish_user_paths /usr/local/bin/
 
   #Adding texlive to path for vscode latex workshop
   set -gx PATH $PATH /opt/homebrew/opt/texlive/bin
