aad0b3956e1061e3cbf7faf2a4c2e15d38daab90
faustozamparelli <fausto.zamparelli@gmail.com>
2024-05-18 00:01:41 +0200

    nvim git change

--- .config/nvim/lua/plugins/vimfugitive.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"tpope/vim-fugitive",
-	config = function()
-		vim.api.nvim_set_keymap("n", "<C-g>", ":Git<CR>", { noremap = true, silent = true })
-		vim.api.nvim_set_keymap("n", "<leader>gl", ":Gclog<CR>", { noremap = true, silent = true })
-	end,
-}
