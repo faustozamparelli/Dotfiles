e8648ebc7f6f1e8f8d96b23bb0e7c4a16017b05f
faustozamparelli <fausto.zamparelli@gmail.com>
2024-10-16 01:09:40 +0200

    oil show hidden files nvim

--- .config/nvim/lua/plugins/default.lua
+++ .config/nvim/lua/plugins/default.lua
@@ -25,6 +25,9 @@ return {
           ["<C-c>"] = "actions.close",
           ["s"] = "actions.change_sort",
         },
+        view_options = {
+          show_hidden = true,
+        }
       })
       vim.keymap.set("n", "'", "<CMD>Oil<CR>", { desc = "Open parent directory" })
     end,
