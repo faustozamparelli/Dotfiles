dfe60220c27eb57a1da8b448eb3902aa5bbbcdd9
faustozamparelli <fausto.zamparelli@gmail.com>
2025-10-10 13:34:56 +0200

    changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -55,12 +55,12 @@ alias nn nvim_configuration_swticher.sh
 
 # thefuck
 if type -q thefuck
-    thefuck --alias | source
+	thefuck --alias | source
 end
 
 # zoxide
 if type -q zoxide
-    zoxide init fish | source
+	zoxide init fish | source
 end
 
 # fifc defaults
@@ -108,48 +108,48 @@ alias bare "/opt/homebrew/bin/git --git-dir=$HOME/.config/git/dotfiles --work-tr
 # ----------------------------
 
 function cd
-    builtin cd $argv
-    if set -q TMUX
-        if tmux has-session >/dev/null 2>&1
-            tmux rename-window (basename $PWD)
-        end
-    end
+	builtin cd $argv
+	if set -q TMUX
+		if tmux has-session >/dev/null 2>&1
+			tmux rename-window (basename $PWD)
+		end
+	end
 end
 
 function z_tmux
-    __zoxide_z $argv
-    if test $status -eq 0
-        if set -q TMUX
-            if tmux has-session >/dev/null 2>&1
-                tmux rename-window (basename $PWD)
-            end
-        end
-    end
+	__zoxide_z $argv
+	if test $status -eq 0
+		if set -q TMUX
+			if tmux has-session >/dev/null 2>&1
+				tmux rename-window (basename $PWD)
+			end
+		end
+	end
 end
 
 function zn
-    __zoxide_z $argv
-    if test $status -eq 0
-        if set -q TMUX
-            if tmux has-session >/dev/null 2>&1
-                tmux rename-window (basename $PWD)
-            end
-        end
-        n .
-    else
-        echo "Directory not found!"
-    end
+	__zoxide_z $argv
+	if test $status -eq 0
+		if set -q TMUX
+			if tmux has-session >/dev/null 2>&1
+				tmux rename-window (basename $PWD)
+			end
+		end
+		n .
+	else
+		echo "Directory not found!"
+	end
 end
 
 function n
-    if test (count $argv) -gt 0 -a "$argv[1]" != "."
-        if set -q TMUX
-            tmux rename-window (basename -- $argv[1])
-        end
-        nvim $argv
-    else
-        nvim .
-    end
+	if test (count $argv) -gt 0 -a "$argv[1]" != "."
+		if set -q TMUX
+			tmux rename-window (basename -- $argv[1])
+		end
+		nvim $argv
+	else
+		nvim .
+	end
 end
 
 # curl + jq helper: split args with -- (curl args first, then --, then jq args)
@@ -172,74 +172,74 @@ end
 # end
 
 function gsp
-    git status -s --porcelain
+	git status -s --porcelain
 end
 
 function gcp
-    git pull --ff-only
-    set default_msg (git status -s --porcelain)
-    read -P "Commit message (leave blank to use status summary): " msg
-    if test -z "$msg"
-        set msg $default_msg
-    end
-    git add .
-    git commit -m "$msg"
-    git push
+	git pull --ff-only
+	set default_msg (git status -s --porcelain)
+	read -P "Commit message (leave blank to use status summary): " msg
+	if test -z "$msg"
+		set msg $default_msg
+	end
+	git add .
+	git commit -m "$msg"
+	git push
 end
 
 function gc
-    set default_msg (git status -s --porcelain)
-    read -P "Commit message (leave blank to use status summary): " msg
-    if test -z "$msg"
-        set msg $default_msg
-    end
-    git add .
-    git commit -m "$msg"
+	set default_msg (git status -s --porcelain)
+	read -P "Commit message (leave blank to use status summary): " msg
+	if test -z "$msg"
+		set msg $default_msg
+	end
+	git add .
+	git commit -m "$msg"
 end
 
 function gcl
-    if test (count $argv) -ge 1
-        set repo_url "https://github.com/$argv[1]"
-        if test (count $argv) -gt 1
-            git clone $repo_url $argv[2]
-        else
-            git clone $repo_url
-        end
-    else
-        echo "Usage: gcl user/repo [dest]"
-    end
+	if test (count $argv) -ge 1
+		set repo_url "https://github.com/$argv[1]"
+		if test (count $argv) -gt 1
+			git clone $repo_url $argv[2]
+		else
+			git clone $repo_url
+		end
+	else
+		echo "Usage: gcl user/repo [dest]"
+	end
 end
 
 function yy
-    set tmp (mktemp -t "yazi-cwd.XXXXXX")
-    yazi $argv --cwd-file="$tmp"
-    if set cwd (cat -- "$tmp"); and test -n "$cwd" ; and test "$cwd" != "$PWD"
-        cd -- "$cwd"
-    end
-    rm -f -- "$tmp"
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (cat -- "$tmp"); and test -n "$cwd" ; and test "$cwd" != "$PWD"
+		cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
 end
 
 function server
-    set current_dir (pwd)
-    if test (count $argv) -gt 0
-        set start_file $argv[1]
-    else
-        set start_file ""
-    end
-    live-server --mount=/:"$current_dir" "$start_file"
+	set current_dir (pwd)
+	if test (count $argv) -gt 0
+		set start_file $argv[1]
+	else
+		set start_file ""
+	end
+	live-server --mount=/:"$current_dir" "$start_file"
 end
 
 function cpmcstudio
-    scp -i ~/.ssh/mcpro faustozamparelli@192.168.1.123:$argv[1] $argv[2]
+	scp -i ~/.ssh/mcpro faustozamparelli@192.168.1.123:$argv[1] $argv[2]
 end
 
 function cpmcpro
-    scp -i ~/.ssh/mcstudio faustozamparelli@192.168.1.216:$argv[1] $argv[2]
+	scp -i ~/.ssh/mcstudio faustozamparelli@192.168.1.216:$argv[1] $argv[2]
 end
 
 # g++ wrapper using Homebrew 15
 function g++
-    /opt/homebrew/bin/g++-15 -std=c++17 -I$HOME/.config/cppheaders $argv
+    /opt/homebrew/bin/g++-15 -std=c++23 -I$HOME/.config/cppheaders $argv
 end
 alias gcc="/opt/homebrew/bin/gcc-15"
 alias cpp="/opt/homebrew/bin/cpp-15"
