7a7e2ac1978ebb6b243cae58dc9d555239bf5b22
fast <fausto.zamparelli@gmail.com>
2026-02-11 23:09:37 +0100

    changes

--- .config/sketchybar/colors.sh
+++ .config/sketchybar/colors.sh
@@ -1,53 +1,11 @@
 #!/bin/bash
 
-
-export WHITE=0xffffffff
-export BLACK=0xff000000
-export GRAY=0xff808080
-
-# -- Teal Scheme --
-# export BAR_COLOR=0xff001f30
-# export ITEM_BG_COLOR=0xff003547
-# export ACCENT_COLOR=0xff2cf9ed
-
-# -- Gray Scheme --
-# export BAR_COLOR=0xff101314
-# export ITEM_BG_COLOR=0xff353c3f
-# export ACCENT_COLOR=0xffffffff
-
-# -- Purple Scheme --
-# export BAR_COLOR=0xff140c42
-# export ITEM_BG_COLOR=0xff2b1c84
-# export ACCENT_COLOR=0xffeb46f9
-
-# -- Red Scheme ---
-# export BAR_COLOR=0xff23090e
-# export ITEM_BG_COLOR=0xff591221
-# export ACCENT_COLOR=0xffff2453
-
-# -- Blue Scheme --- 
-# export BAR_COLOR=0xff021254
-# export ITEM_BG_COLOR=0xff093aa8
-# export ACCENT_COLOR=0xff15bdf9
-
-# -- Green Scheme --
-# export BAR_COLOR=0xff003315
-# export ITEM_BG_COLOR=0xff008c39
-# export ACCENT_COLOR=0xff1dfca1
-
-
-# -- Orange Scheme --
-# export BAR_COLOR=0xff381c02
-# export ITEM_BG_COLOR=0xff99440a
-# export ACCENT_COLOR=0xfff97716
-
-# -- Yellow Scheme --
-export BAR_COLOR=0x00ffffff
-export RIGHT_BG_COLOR=0xff8948ff
-export LEFT_BG_COLOR=0xff8948ff
-export LEFT_COLOR=$BLACK
-export LEFT_UNSELECTED=$WHITE
-export MUSIC=$WHITE
-export MUSIC_BG=0x00ffffff
-
-
+export BAR_BG=0x00000000
+export ITEM_BG=0xff0c0c0e
+export ITEM_BG_ALT=0xff15151a
+export ACCENT=0xff9c6bff
+export ACCENT_DARK=0xff32283f
+export TEXT=0xffffffff
+export MUTED=0xffc9c9d1
+export WARNING=0xffffb74d
+export GOOD=0xff7fe7c4
--- .config/sketchybar/items/temperature.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item temperature right \
-           --set temperature update_freq=20 \
-                             icon=􀇬  \
-                             script="$PLUGIN_DIR/temperature.sh"
