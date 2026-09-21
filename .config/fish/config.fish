650589236867575d6d7b6bf92881cb98f8021a23
fast <fausto.zamparelli@gmail.com>
2026-09-21 15:20:14 +0200

    changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -97,3 +97,7 @@ function bcp --description "Bare add -u, commit, and push (subject + optional de
     end
     bare push
 end
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
